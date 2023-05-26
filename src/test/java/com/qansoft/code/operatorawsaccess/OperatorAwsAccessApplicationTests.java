2a075b92118ea155fe2c33722d4b4d1608002c2e
Quan Nguyen <quan.nguyen@virtimo.de>
2023-05-26 15:15:10 +0200

    init

---
+++ src/test/java/com/qansoft/code/operatorawsaccess/OperatorAwsAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qansoft.code.operatorawsaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperatorAwsAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
